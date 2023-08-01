3917df9e145c0b1c91ad9e7507b6e479412bb4cd
LailaIH <112259471+LailaIH@users.noreply.github.com>
2023-08-01 03:20:06 +0300

    fourth commit

--- project/settings.py
+++ project/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
-import dj_database_url
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
@@ -148,4 +148,3 @@ REST_FRAMEWORK = {
     ]
 }
 
-django_heroku.settings(locals())
